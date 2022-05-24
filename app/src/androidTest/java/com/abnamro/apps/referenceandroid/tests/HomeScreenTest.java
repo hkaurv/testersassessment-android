a2d218c5f6e3e2cd954cd260b73acbd41eab59c3
hkaurv <harjotvirk16@gmail.com>
2022-05-24 10:44:51 +0200

    add files

---
+++ app/src/androidTest/java/com/abnamro/apps/referenceandroid/tests/HomeScreenTest.java
@@ -0,0 +1,42 @@
+package com.abnamro.apps.referenceandroid.tests;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.abnamro.apps.referenceandroid.screens.homeScreen;
+import com.abnamro.apps.referenceandroid.utils.SmokeTest;
+
+/* HomeScreen class contains UI instrumented tests for ReferenceAndroid App
+that verifies UI elements and their behavior correctness
+ */
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class HomeScreenTest extends BaseTest {
+    homeScreen homescreen = new homeScreen();
+
+        @Test
+        @SmokeTest
+        public void verifyHomeScreenText() {
+            homescreen.homeScreenText_check();
+        }
+
+        @Test
+        public void verifyUserCanClickOnOverflowMenu() {
+        homescreen.clickOverflowMenu();
+        }
+
+        @Test
+        @SmokeTest
+        public void verifyUserCanClickOnSettings() {
+        homescreen.clickOverflowMenu();
+        homescreen.clickSettings();
+        }
+
+        @Test
+        public void verifySnackbarOnClickingFloatingIcon() {
+            homescreen.floatingButtonHomescreen_tap();
+            homescreen.verifySnackBarText();
+        }
+    }
+
