15e64c1c235a48d7d4a05a9bd91e4bedddc1d7a1
hkaurv <harjotvirk16@gmail.com>
2022-05-24 23:20:32 +0200

    Add automation tests

--- app/src/androidTest/java/com/abnamro/apps/referenceandroid/tests/BaseTest.java
+++ app/src/androidTest/java/com/abnamro/apps/referenceandroid/tests/BaseTest.java
@@ -3,7 +3,7 @@ package com.abnamro.apps.referenceandroid.tests;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import com.abnamro.apps.referenceandroid.MainActivity;
 import org.junit.Rule;
-/*This Base class has teh rule which can be shared among different test cases as we add new screens.
+/*This Base class has the rule which can be shared among different test cases as we add new screens.
 Any activity which needs to be done before or after the test can be added here which will be common to all tests.
 it can be extended to sub classes  using the keyword "extends". */
 public class BaseTest {
